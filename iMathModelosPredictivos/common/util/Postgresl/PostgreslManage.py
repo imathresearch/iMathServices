331092085ed99fb882337b334675528da074a369
Inigo Zubizarreta <Inigo Zubizarreta>
2015-11-24 17:09:17 +0100

    Postgresl nearly integrated at iMath Services

--- iMathModelosPredictivos/common/util/Postgresl/PostgreslManage.py
+++ iMathModelosPredictivos/common/util/Postgresl/PostgreslManage.py
@@ -33,6 +33,14 @@ class PostgreslManage(object):
         maxValue = self.ConnectionBBDD.getNextPrimaryKey(table)
         return maxValue
     
+    def setStoreModel(self,table,parameters):
+        
+        self.ConnectionBBDD.setDataModel(table, parameters)
+        
+    def setStoreModelsResults(self,table,data):
+        
+        self.ConnectionBBDD.setDataModelResults(table, data)
+       
     def closeConnection(self):
     
         self.ConnectionBBDD.closeConnection()
